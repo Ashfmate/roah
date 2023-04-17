c7bff5cf30aed80695c51e901222f5a081d91e4c
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-04-18 01:53:02 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace roah.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
